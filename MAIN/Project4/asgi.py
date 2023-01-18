e31138ebd008225498708fba6fe1af14874a15c1
PrezBala <prez5aw@live.com>
2023-01-18 17:48:17 +0000

    Import Django apps, project4 and main folder created and html imported and saved within templates folder

--- MAIN/Project4/asgi.py
+++ MAIN/Project4/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for django_forum project.
+ASGI config for Project4 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_forum.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project4.settings')
 
 application = get_asgi_application()
