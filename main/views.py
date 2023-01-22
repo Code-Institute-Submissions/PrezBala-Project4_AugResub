d4ebd6a46cf6d883eb7e3b921b71895108627fb5
PrezBala <prez5aw@live.com>
2023-01-22 15:51:25 +0000

    additional def home variables added within views.py

--- main/views.py
+++ main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html", {})
+
+
+def home(request):
+    return render(request, "detail.html", {})
+
+
+def home(request):
+    return render(request, "posts.html", {})
+
+
+def home(request):
+    return render(request, "index.html", {})
