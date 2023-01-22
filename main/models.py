c915dfc0f2a4198f4235e597f804fa6c1977794e
PrezBala <prez5aw@live.com>
2023-01-22 17:04:00 +0000

    models.py under main directory add class for author and post, define django import for slugify and also max character lengths

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=40, blank=True)
+    slug = slug = models.SlugField(max_length=400, unique=True, blank=True)
+    bio = models.TextField()
+    points = models.IntegerField(default=0)
+    profile_pic = 
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.fullname)
+        super(Post, self).save(*args, **kwargs)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=400)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    user = models.ForeignKey()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
