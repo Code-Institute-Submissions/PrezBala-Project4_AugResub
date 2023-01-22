643d7819dbdb4348b28a1d39f3eb99567de7405d
PrezBala <prez5aw@live.com>
2023-01-22 17:11:08 +0000

    add points and profile pic to models.py, import django resized and also set variables

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth import get_user_model
+from django_resized import ResizedImageField
 
 User = get_user_model()
 
@@ -11,8 +12,7 @@ class Author(models.Model):
     slug = slug = models.SlugField(max_length=400, unique=True, blank=True)
     bio = models.TextField()
     points = models.IntegerField(default=0)
-    profile_pic = 
-
+    profile_pic = ResizedImageField(size=[50, 80], quality=100, upload_to="authors", default=None, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
