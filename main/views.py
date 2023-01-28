135d06c01087a1041ee5cf219667f42763d95ad5
PrezBala <prez5aw@live.com>
2023-01-28 20:33:33 +0000

    revert changes to import to troubleshoot errors

--- main/views.py
+++ main/views.py
@@ -3,11 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html", {})
-
-
-def detail(request):
-    return render(request, "detail.html", {})
-
-
-def posts(request):
-    return render(request, "posts.html", {})
