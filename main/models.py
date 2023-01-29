a22893d7d57f2afc1853c2da76207483de64d447
PrezBala <prez5aw@live.com>
2023-01-29 14:37:24 +0000

    add content, categories, date to class posts under models.py also referenced slugify to those classes

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth import get_user_model
 from django_resized import ResizedImageField
+from tinymce.models import HTMLField
 
 User = get_user_model()
 
@@ -10,7 +11,7 @@ class Author(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=40, blank=True)
     slug = slug = models.SlugField(max_length=400, unique=True, blank=True)
-    bio = models.TextField()
+    bio = HTMLField()
     points = models.IntegerField(default=0)
     profile_pic = ResizedImageField(
         size=[50, 80],
@@ -21,16 +22,39 @@ class Author(models.Model):
         blank=True
     )
 
+    def __str__(self):
+        return self.fullname
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.fullname)
         super(Post, self).save(*args, **kwargs)
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=400)
     slug = models.SlugField(max_length=400, unique=True, blank=True)
     user = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = HTMLField()
+    categories = models.ManyToManyField(Category)
+    date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         if not self.slug:
