3d613f9982ab473a15f7d8b200bee1fce1a8129a
PrezBala <prez5aw@live.com>
2023-01-29 22:38:52 +0000

    add MEDIA root and MEDIA url to settings.py and also added if settings debug mode enable media/static root

--- Project4/urls.py
+++ Project4/urls.py
@@ -4,9 +4,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('tinymce/', include('tinymce.urls')),
     path('hitcount/', include('hitcount.urls', namespace='hitcount')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
