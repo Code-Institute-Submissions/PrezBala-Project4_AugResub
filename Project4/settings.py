3671128f4d8fbc1477e734fe74b3a1263af743e3
PrezBala <prez5aw@live.com>
2023-02-05 15:23:27 +0000

    add crispy text within signup.html and also load crispy forms tag

--- Project4/settings.py
+++ Project4/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "hitcount",
     "taggit",
     "register",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
