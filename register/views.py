a2dffc8da2bef5a3767ab71a8e452a4321043dc1
PrezBala <prez5aw@live.com>
2023-02-05 16:50:03 +0000

    create update.html and add tags/reference, also add form method POST

--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from register.forms import UpdateForm
 
 
 def signup(request):
