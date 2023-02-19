1054ab8187e91d13782adc0a245b8fd47a15c973
PrezBala <prez5aw@live.com>
2023-02-19 20:00:52 +0000

    add search within urls.py under main to from views and also to url patterns

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import (
-    home, detail, posts, create_post, latest_posts)
+    home, detail, posts, create_post, latest_posts,
+    search_result,)
 
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path("posts/<slug>/", posts, name="posts"),
     path("create_post", create_post, name="create_post"),
     path("latest_posts", latest_posts, name="latest_posts"),
+    path("search", search_result, name="search_result"),
 ]
--- main/views.py
+++ main/views.py
@@ -109,3 +109,8 @@ def latest_posts(request):
     }
 
     return render(request, "latest-posts.html", context)
+
+
+def search_result(request):
+
+    return render(request, "search.html")
