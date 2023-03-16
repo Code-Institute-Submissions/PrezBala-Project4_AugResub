434a21d69fcefb2ce27385d1ea4d6054fa27be13
PrezBala <prez5aw@live.com>
2023-03-16 23:40:37 +0000

    amend video-wrapper within style.css as video was stretched incorrectly across the screen

--- Project4/settings.py
+++ Project4/settings.py
@@ -20,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-oeztf)4va2syt)ddhyf@)on2n4q%wzfh8%' +
-    'a20d%69vag89e8$3'
-)
+SECRET_KEY = 'django-insecure-oeztf)4va2syt)ddhyf@)on2n4q%wzfh8%a20d%69vag89e8$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
